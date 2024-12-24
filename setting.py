d79fa044eb33ec19f3b4e263f09b5eddf628ac13
Alvi399 <muhammadalvi.23338@mhs.unesa.ac.id>
2024-12-24 11:52:30 +0700

    up

--- setting.py
+++ setting.py
@@ -1,10 +1,9 @@
 import pygame
-import sys
 
 # Initialize Pygame
 pygame.init()
 
-# Set up display
+#set up display
 width, height = 800, 600
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Pygame Example')
@@ -13,22 +12,39 @@ pygame.display.set_caption('Pygame Example')
 black = (0, 0, 0)
 white = (255, 255, 255)
 
+# Set up game variables
+x, y = 150, 150
+velocity = 1    
+
 # Main game loop
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x -= velocity
+    if keys[pygame.K_RIGHT]:
+        x += velocity
+    if keys[pygame.K_UP]:
+        y -= velocity
+    if keys[pygame.K_DOWN]:
+        y += velocity
+    if keys[pygame.K_ESCAPE]:
+        x = 150
+        y = 150
     # Fill the background with white
     window.fill(white)
 
     # Draw a black rectangle
-    pygame.draw.rect(window, black, (150, 150, 100, 50))
+    pygame.draw.rect(window, black, (x, y, 100, 50))
 
     # Update the display
     pygame.display.flip()
 
 # Quit Pygame
 pygame.quit()
-sys.exit()
+
+#sys.exit()
